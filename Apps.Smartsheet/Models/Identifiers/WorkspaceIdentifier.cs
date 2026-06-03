c1b34af37195efde122ca4f3890afb858eeb7a79
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-06-03 10:11:19 +0300

    add missing data handler to WorkspaceIdentifier

--- Apps.Smartsheet/Models/Identifiers/WorkspaceIdentifier.cs
+++ Apps.Smartsheet/Models/Identifiers/WorkspaceIdentifier.cs
@@ -1,9 +1,10 @@
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Smartsheet.Models.Identifiers;
 
 public class WorkspaceIdentifier
 {
-    [Display("Workspace ID")]
+    [Display("Workspace ID"), DataSource(typeof(WorkspaceIdentifier))]
     public string WorkspaceId { get; set; } = string.Empty;
 }
