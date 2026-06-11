55ffbe9a65c2ce689a419dfb105bd990e8a63c81
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-06-11 15:12:24 +0300

    remove WebhookLogger

--- Apps.Smartsheet/Helper/Webhook/WebhookHelper.cs
+++ Apps.Smartsheet/Helper/Webhook/WebhookHelper.cs
@@ -11,27 +11,21 @@ public static class WebhookHelper
 {
     public static ProcessedEvent ProcessEvent(WebhookRequest request, string targetObjectType, string targetEventType)
     {
-        WebhookLogger.Log(request.Body);
         var payloadString = request.Body.ToString() ?? 
                             throw new PluginApplicationException("Payload string was empty");
-        WebhookLogger.Log(payloadString);
         
         var payload = JsonConvert.DeserializeObject<WebhookPayloadEntity>(payloadString);
-        WebhookLogger.Log(payload);
-
-        if (!string.IsNullOrEmpty(payload.Challenge))
+        if (!string.IsNullOrEmpty(payload?.Challenge))
         {
             var handshakeResponse = new { smartsheetHookResponse = payload.Challenge };
             var handshakeJson = JsonConvert.SerializeObject(handshakeResponse);
-            WebhookLogger.Log(handshakeJson);
             
             var httpResponse = new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StringContent(handshakeJson, Encoding.UTF8, "application/json")
             };
             httpResponse.Headers.Add("Smartsheet-Hook-Response", payload.Challenge);
-            WebhookLogger.Log(httpResponse);
-        
+            
             return new(httpResponse, null); 
         }
 
